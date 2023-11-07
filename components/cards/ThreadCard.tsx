6f3cd46b4d9343b5df798baf64e0c3a9282a2978
tienv1per <tienv1perds@gmail.com>
2023-11-08 06:00:13 +0700

    component thread card

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ThreadCardProps {
+	id: string;
+	currentUserId: string;
+	parentId: string | null;
+	content: string;
+	author: {
+		name: string;
+		image: string;
+		id: string;
+	};
+	community: {
+		id: string;
+		name: string;
+		image: string;
+	} | null;
+	createdAt: string;
+	comments: {
+		author: {
+			image: string;
+		}
+	}[];
+	isComment?: boolean;
+};
+
+const ThreadCard = ({
+	id,
+	currentUserId,
+	parentId,
+	content,
+	author,
+	community,
+	createdAt,
+	comments
+}: ThreadCardProps) => {
+  return (
+    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+			<div className="flex item-start justify-between">
+				<div className="flex w-full flex-1 flex-row gap-4">	
+					<div className="flex flex-col items-center">
+						<Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+							<Image
+								src={author.image}
+								alt="Profile image"
+								fill
+								className="cursor-pointer rounded-full"
+							/>
+						</Link>
+						<div className="thread-card_bar" />
+					</div>
+					<div className="flex w-full flex-col">
+						<Link href={`/profile/${author.id}`}>
+							<h4 className="cursor-pointer text-base-semibold text-light-1">
+								{author.name}
+							</h4>
+						</Link>
+						<p className="mt-2 text-small-regular text-light-2">
+							{content}
+						</p>
+					</div>
+				</div>
+			</div>
+		</article>
+  );
+};
+
+export default ThreadCard;
