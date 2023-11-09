a0f0da8de44e89490a5bf1f6416e20ff5f2c2b31
tienv1per <tienv1perds@gmail.com>
2023-11-10 01:26:02 +0700

    feature/profile page

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,19 @@
+interface ThreadsTabProps {
+	currentUserId: string;
+	accountId: string;
+	accountType: string;
+}
+
+const ThreadsTab = async({currentUserId, accountId, accountType}: ThreadsTabProps) => {
+	// TODO: fetch profile threads
+
+  return (
+    <div>
+			<section>
+
+			</section>
+		</div>
+  );
+};
+
+export default ThreadsTab;
